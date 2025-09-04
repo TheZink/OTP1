722fc59be7f9f0e44819ca38c4d850ec89c623b7
TheZink <ilkka.sinkonen@gmail.com>
2025-09-04 09:33:31 +0300

    Reorganize SQL script: move AttendanceInfo table creation and adjust join tables for clarity

--- scripts/sql_script.sql
+++ scripts/sql_script.sql
@@ -13,22 +13,6 @@ CREATE TABLE IF NOT EXISTS Course(
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE IF NOT EXISTS course_user_join(
-    user_id int NOT NULL,
-    course_id INT NOT NULL,
-    PRIMARY KEY (user_id, course_id),
-    FOREIGN KEY (user_id) REFERENCES Users(id),
-    FOREIGN KEY (course_id) REFERENCES Course(id)
-);
-
-CREATE TABLE IF NOT EXISTS course_staff_join(
-    staff_id int NOT NULL,
-    course_id INT NOT NULL,
-    PRIMARY KEY (staff_id, course_id),
-    FOREIGN KEY (staff_id) REFERENCES Staff(id),
-    FOREIGN KEY (course_id) REFERENCES Course(id)
-);
-
 CREATE TABLE IF NOT EXISTS Staff(
     id INT PRIMARY KEY AUTO_INCREMENT,
     staff_name VARCHAR(50) NOT NULL,
@@ -55,16 +39,32 @@ CREATE TABLE IF NOT EXISTS Attendance(
     FOREIGN KEY (staff_id) REFERENCES Staff(id)
 );
 
+CREATE TABLE IF NOT EXISTS AttendanceInfo(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    info VARCHAR(250),
+    approve BOOLEAN DEFAULT FALSE
+);
+
+CREATE TABLE IF NOT EXISTS course_user_join(
+    user_id int NOT NULL,
+    course_id INT NOT NULL,
+    PRIMARY KEY (user_id, course_id),
+    FOREIGN KEY (user_id) REFERENCES Users(id),
+    FOREIGN KEY (course_id) REFERENCES Course(id)
+);
+
+CREATE TABLE IF NOT EXISTS course_staff_join(
+    staff_id int NOT NULL,
+    course_id INT NOT NULL,
+    PRIMARY KEY (staff_id, course_id),
+    FOREIGN KEY (staff_id) REFERENCES Staff(id),
+    FOREIGN KEY (course_id) REFERENCES Course(id)
+);
+
 CREATE TABLE IF NOT EXISTS Attendance_info_join(
     atten_id INT,
     info_id INT,
     PRIMARY KEY (atten_id, info_id),
     FOREIGN KEY (atten_id) REFERENCES Attendance(id),
     FOREIGN KEY (info_id) REFERENCES AttendanceInfo(id)
-);
-
-CREATE TABLE IF NOT EXISTS AttendanceInfo(
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    info VARCHAR(250),
-    approve BOOLEAN DEFAULT FALSE
 );
