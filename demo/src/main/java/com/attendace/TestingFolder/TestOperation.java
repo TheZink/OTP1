08ad711faec21cae3e087b8048096d17282b1112
veikk <valttejo@metropolia.fi>
2025-09-12 10:22:58 +0300

    Add JUnit testing framework and create initial test cases for addition operation

---
+++ demo/src/main/java/com/attendace/TestingFolder/TestOperation.java
@@ -0,0 +1,7 @@
+package com.attendace.TestingFolder;
+
+public class TestOperation {
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
---
+++ demo/src/test/java/com/attendace/TestingFolder/TestOperationTest.java
@@ -0,0 +1,15 @@
+package com.attendace.TestingFolder;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestOperationTest {
+
+    @Test
+    void add() {
+        TestOperation testOperation = new TestOperation();
+        assertEquals(5, testOperation.add(2, 3));
+        assertNotEquals(6, testOperation.add(2, 3));
+    }
+}
