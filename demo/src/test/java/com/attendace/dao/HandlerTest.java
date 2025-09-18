98592b83062c2cdcbcbb84ba35eb71c19e81c97b
TheZink <ilkka.sinkonen@gmail.com>
2025-09-18 13:48:47 +0300

    Add initial unit tests for Handler class

---
+++ demo/src/test/java/com/attendace/dao/HandlerTest.java
@@ -0,0 +1,25 @@
+package com.attendace.dao;
+
+import org.junit.jupiter.api.Test;
+
+public class HandlerTest {
+    @Test
+    void testCanProcess() {
+
+    }
+
+    @Test
+    void testHandle() {
+
+    }
+
+    @Test
+    void testProcess() {
+
+    }
+
+    @Test
+    void testSetNextHandler() {
+
+    }
+}
