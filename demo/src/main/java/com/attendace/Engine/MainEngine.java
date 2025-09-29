7722a00d8de02c1ec8171baf02e92351bd82bf7b
Daniel <100704382+Ristler@users.noreply.github.com>
2025-09-29 13:24:21 +0300

    Refactor login functionality and introduce MainEngine for scene management

---
+++ demo/src/main/java/com/attendace/Engine/MainEngine.java
@@ -0,0 +1,40 @@
+package com.attendace.Engine;
+
+import com.attendace.View.MainInterfaceController;
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
+
+import java.util.Objects;
+
+public class MainEngine {
+    MainInterfaceController mainInterfaceController = new MainInterfaceController();
+
+
+    public MainEngine() {
+
+    }
+
+    public void runEngine() throws IOException {
+        //emailfield.getScene().getWindow().hide();
+
+        Parent root = FXMLLoader.load(
+                Objects.requireNonNull(getClass().getResource("/fxml/ProjectInterface.fxml"))
+        );
+
+        Stage stage = new Stage();
+        stage.setTitle("Interface");
+        stage.setScene(new Scene(root));
+        stage.showAndWait();
+
+        mainInterfaceController.initialize();
+    }
+}
