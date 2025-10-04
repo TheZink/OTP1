74040c7e2bd4cea44036cda4241c59e606196899
TheZink <ilkka.sinkonen@gmail.com>
2025-10-04 16:40:19 +0300

    Add AdminInterfaceController and AdminPage.fxml for admin functionalities

---
+++ demo/src/main/java/com/attendace/View/AdminInterfaceController.java
@@ -0,0 +1,57 @@
+package com.attendace.View;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+
+public class AdminInterfaceController {
+    @FXML
+    Button viewStaff;
+
+    @FXML
+    Button createStaff;
+
+    @FXML
+    Button modifyStaff;
+
+    @FXML
+    Button deleteStaff;
+
+    @FXML
+    Button viewStudent;
+
+    @FXML
+    Button createStudent;
+
+    @FXML
+    Button modifyStudent;
+
+    @FXML
+    Button deleteStudent;
+
+    @FXML
+    Button viewCourse;
+
+    @FXML
+    Button createCourse;
+
+    @FXML
+    Button modifyCourse;
+
+    @FXML
+    Button deleteCourse;
+
+    @FXML
+    Button viewAtten;
+
+    @FXML
+    Button modifyAtten;
+
+    @FXML
+    Button saveButton;
+
+    @FXML
+    Button cancelButton;
+
+}
