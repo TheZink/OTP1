7c1f9e73057cedff0567817555b380b4152682d8
Daniel <100704382+Ristler@users.noreply.github.com>
2025-12-01 22:30:14 +0200

    Add unit tests for UserCourseController to validate user-course creation functionality

---
+++ demo/src/test/java/com/attendace/controller/UserCourseControllerTest.java
@@ -0,0 +1,26 @@
+package com.attendace.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserCourseControllerTest {
+
+    UserCourseController userCourseController;
+
+    @BeforeEach
+    void setUp() {
+        userCourseController = new UserCourseController();
+    }
+
+    @Test
+    void createUserCourse() {
+        assertTrue(userCourseController.createUserCourse(5, 6));
+
+    }
+
+    @Test
+    void getUserCourses() {
+    }
+}
